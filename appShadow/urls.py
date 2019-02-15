818380187fb34b6fcdd1d762b9a12a08c213774b
Earvendakil <earvendakil@gmail.com>
2019-02-15 16:18:36 +0300

    'sixth'

--- appShadow/urls.py
+++ appShadow/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
+    url(r'^$', 'views.index', name = 'index'),
     ]
--- project/urls.py
+++ project/urls.py
@@ -1,14 +1,13 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path
 
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    path('shadow/', include('appShadow.urls')),
+    url(r'^shadow/', include('appShadow.urls')),
     url(r'^admin/', include(admin.site.urls)),
     
 ]
