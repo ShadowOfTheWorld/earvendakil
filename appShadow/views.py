4c8607b7a97b957a937f7c2cbb850867383a2aa8
Earvendakil <earvendakil@gmail.com>
2019-02-15 16:39:03 +0300

    eighth

--- appShadow/views.py
+++ appShadow/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import telegram
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, World!")
+    bot = telegram.Bot(token='747350126:AAFSlYXgWRmJ9HowkXvVBpxsA_ZgPSpxvMU')
+    return HttpResponse(bot.get_me())
