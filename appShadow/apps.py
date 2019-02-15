d995d7d1d72b44aa336231b8cc7a17e2531876fc
Earvendakil <earvendakil@gmail.com>
2019-02-15 15:36:59 +0300

    first commit

---
+++ appShadow/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppshadowConfig(AppConfig):
+    name = 'appShadow'
---
+++ projectShadow/urls.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.conf.urls import include, url
+from django.contrib import admin
+
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'project.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    path('shadow/', include('appShadow.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
